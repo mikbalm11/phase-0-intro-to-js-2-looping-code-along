be41b9dc835baf7f87004590f9a01d58e0df04e7
Mustafa Mehmedoglu <mikbalm@gmail.com>
2024-04-01 01:00:23 -0400

    tests successful

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(arr, str) {
+    const messages = [];
+    for (let i = 0; i < arr.length; i++) {
+        messages.push(`Thank you, ${arr[i]}, for the wonderful ${str} gift!`);
+    }
+    return messages;
+}
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num);
+        num -= 1;
+    }
+}
